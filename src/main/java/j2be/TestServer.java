45d65d6a3d903e4764ea9fdb6b00682720813638
Azteched <Aztechedmc@gmail.com>
2018-09-30 15:40:50 +0100

    First server test to see packets sent by Bedrock Client

---
+++ src/main/java/j2be/TestServer.java
@@ -0,0 +1,54 @@
+package j2be;
+
+import java.util.Random;
+
+import com.whirvis.jraknet.RakNet;
+import com.whirvis.jraknet.RakNetException;
+import com.whirvis.jraknet.RakNetPacket;
+import com.whirvis.jraknet.identifier.MinecraftIdentifier;
+import com.whirvis.jraknet.server.RakNetServer;
+import com.whirvis.jraknet.server.RakNetServerListener;
+import com.whirvis.jraknet.session.RakNetClientSession;
+import com.whirvis.jraknet.windows.UniversalWindowsProgram;
+
+public class TestServer {
+	public static void main(String[] args)throws RakNetException{
+		// Add loopback exemption for Minecraft
+		if (!UniversalWindowsProgram.MINECRAFT.addLoopbackExempt()) {
+			System.out.println("Failed to add loopback exemption for Minecraft");
+		}
+	
+		// Create server
+		RakNetServer server = new RakNetServer(19132, 10,
+				new MinecraftIdentifier("JRakNet Example Server", 137, "1.2", 0, 10,
+						new Random().nextLong() /* Server broadcast ID */, "New World", "Survival"));
+	
+		// Add listener
+		server.addListener(new RakNetServerListener() {
+	
+			// Client connected
+			@Override
+			public void onClientConnect(RakNetClientSession session) {
+				System.out.println("Client from address " + session.getAddress() + " has connected to the server");
+			}
+	
+			// Client disconnected
+			@Override
+			public void onClientDisconnect(RakNetClientSession session, String reason) {
+				System.out.println("Client from address " + session.getAddress()
+						+ " has disconnected from the server for the reason \"" + reason + "\"");
+			}
+	
+			// Packet received
+			@Override
+			public void handleMessage(RakNetClientSession session, RakNetPacket packet, int channel) {
+				System.out.println("Client from address " + session.getAddress() + " sent packet with ID "
+						+ RakNet.toHexStringId(packet) + " on channel " + channel);
+			}
+	
+		});
+		server.start();
+		
+
+	};
+}
